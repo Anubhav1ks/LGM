470f50cc3d4f398f9ddcd1a31549e7332c6fe33c
Anubhav <ann@dssa.com>
2021-11-04 13:59:20 +0530

    Initialize project using Create React App

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = (props) => {
+
+  const loadUsers = async()=>{
+    console.log('before');
+    props.setLoading(true);
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const jsonresponse = await response.json();
+    console.log(jsonresponse.data);
+    props.setUsers(jsonresponse.data);
+    setTimeout(() => {
+      props.setLoading(false);
+    }, 3000);
+  }
+  return (
+        <div className="navbar">
+          <h1>TechCurators</h1>
+        <button onClick={loadUsers}>Get Users</button>
+      </div>
+  );
+}
+
+export default Navbar;
